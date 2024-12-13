16a5a1182b2e3f8d4a2b9971c321666152ae1931
Boec <morono0205@gmail.com>
2024-12-14 06:35:07 +0700

    user register api w encrypted pass

--- api/routes/User.js
+++ api/routes/User.js
@@ -22,4 +22,34 @@ userRoute.post('/login', AsynHandler(async (req, res) => {
     }
 }));
 
+userRoute.post('/', AsynHandler(async(req, res)=>{
+    const {name, email, password} = req.body;
+    const existUser = await User.findOne({email});
+    if (existUser) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+    else {
+        const user = await User.create({ name, email, password});
+    }
+
+    if(user) {
+        res.status(201).status({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            createdAt: user.createdAt,
+        });
+    }
+    else {
+        res.status(400);
+        throw new Error("Invalid Username Data");
+    }
+}))
+
+
+
+
+
 module.exports = userRoute;
