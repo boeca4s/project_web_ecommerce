c27f91b564de8aa3cf41c8ec487d67133c0ac992
Boec <morono0205@gmail.com>
2024-12-15 11:40:52 +0700

    user profile update f

--- api/routes/User.js
+++ api/routes/User.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const userRoute = express.Router();
-const AsynHandler = require("express-async-handler");
+const AsyncHandler = require("express-async-handler");
 const User = require("../models/User");
 const genarateToken = require("../tokenGenerate");
 const protect = require("../middleware/Auth");
 
 
-userRoute.post('/login', AsynHandler(async (req, res) => {
+userRoute.post('/login', AsyncHandler(async (req, res) => {
     const {email, password} = req.body;
     const user = await User.findOne({email});
     if(user && (await user.matchPassword(password))) {
@@ -25,7 +25,7 @@ userRoute.post('/login', AsynHandler(async (req, res) => {
     }
 }));
 
-userRoute.post('/', AsynHandler(async(req, res)=>{
+userRoute.post('/', AsyncHandler(async(req, res)=>{
     const {name, email, password} = req.body;
     const existUser = await User.findOne({email});
     if (existUser) {
@@ -51,8 +51,8 @@ userRoute.post('/', AsynHandler(async(req, res)=>{
     }
 }))
 
-//profile data
-userRoute.get("/profile", protect, AsynHandler(async(req, res)=>{
+//authorization profile data
+userRoute.get("/profile", protect, AsyncHandler(async(req, res)=>{
     const user = await User.findById(req.user._id);
     if (user) {
         res.json({ 
@@ -71,6 +71,31 @@ userRoute.get("/profile", protect, AsynHandler(async(req, res)=>{
     }
 }))
 
+//user profile update
+userRoute.put("/profile", protect, AsyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if (req.body.password) {
+            user.password = req.body.password || user.password;
+        }
+        const updatedUser = await user.save();
+        res.json({ 
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            createdAt: updatedUser.createdAt,
+            token: generateToken(updatedUser._id),
 
+        })
+    }
+    else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+}
+));
 
 module.exports = userRoute;
