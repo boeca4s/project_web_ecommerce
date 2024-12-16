2342f19f58e426e03405aa37e0a59c463e1455a0
Boec <morono0205@gmail.com>
2024-12-17 04:35:31 +0700

    Update Auth.js

--- api/middleware/Auth.js
+++ api/middleware/Auth.js
@@ -1,24 +1,25 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
-
-const protect = asyncHandler(async(req, res, next) => {
-    let token;
-    if (req.headers.authorization && req.headers.authorization.startWith("Bearer")) {
-        try {
-                token = req.headers.authorization.split(" ")[1]
-                const decodedToken = just.verify(token, process.JWT_SECRET);
-                request.user = await User.findById(decodedToken.id).select("-password");
-                next();
-            }
-        catch (err) {
-            console.log(err);
-        }
+const User = require("../models/User");
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decodedToken = just.verify(token, process.JWT_SECRET);
+      req.user = await User.findById(decodedToken.id).select("-password");
+      next();
+    } catch (err) {
+      console.log(err);
     }
-    if(!token) {
-        res.status(401);
-        throw new Error("Not authorized!");
-    }
-
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized!");
+  }
 });
 
 module.exports = protect;
