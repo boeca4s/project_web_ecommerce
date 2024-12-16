bc17ff1692bea93a9943af6bf14bc479c092a149
Boec <morono0205@gmail.com>
2024-12-17 04:47:58 +0700

    fix auth 02

--- api/middleware/Auth.js
+++ api/middleware/Auth.js
@@ -5,11 +5,11 @@ const protect = asyncHandler(async (req, res, next) => {
   let token;
   if (
     req.headers.authorization &&
-    req.headers.authorization.startWith("Bearer")
+    req.headers.authorization.startsWith("Bearer")
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
-      const decodedToken = just.verify(token, process.JWT_SECRET);
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decodedToken.id).select("-password");
       next();
     } catch (err) {
@@ -18,7 +18,7 @@ const protect = asyncHandler(async (req, res, next) => {
   }
   if (!token) {
     res.status(401);
-    throw new Error("Not authorized!");
+    throw new Error("Not authorized");
   }
 });
 
